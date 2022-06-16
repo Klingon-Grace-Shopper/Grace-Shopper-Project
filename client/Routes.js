bfffc95a79837d3aa24bd43c70e28a995cd990b3
Jason <is5jasonlin@gmail.com>
2022-06-16 11:47:57 -0400

    im confused

--- client/Routes.js
+++ client/Routes.js
@@ -5,6 +5,7 @@ import Login from "./components/Login";
 import Home from "./components/Home";
 import { me } from "./store";
 import { SingleBook } from "./components/SingleBook";
+import { Cart } from "./components/Cart";
 
 /**
  * COMPONENT
@@ -23,6 +24,7 @@ class Routes extends Component {
           <Route path="/home" component={Home} />
           {/* <Redirect to="/home" /> */}
           <Route path="/books/:id" component={SingleBook} />
+          <Route path="/cart" component={Cart} />
         </Switch>
         {/* {isLoggedIn ? (
           <Switch>
