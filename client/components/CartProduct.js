0cb33345eead722666560f35cea80176c2c5cbed
Jason <is5jasonlin@gmail.com>
2022-06-19 14:28:46 -0400

    can change quantity in cart

--- client/components/CartProduct.js
+++ client/components/CartProduct.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React ,{ useState }from "react";
+import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { deleteBook } from "../store/cart";
 
@@ -6,27 +7,39 @@ export const CartProduct = (props) => {
   const dispatch = useDispatch();
 
   let book = props.book;
-  //need to add in return:
-  //onclick + method to remove book
-  //display price + quantity
+
+  const [quantity, setQuantity] = useState(book.quantity);
+  book.quantity = quantity
   return (
-    <div className="cartProduct">
-      <div className="cartProductInfo">
-        <img src={book.imageUrl} className="cartProductImg" />
-        <span>{book.title}</span>
-        <span>{book.author}</span>
-        <span>
-          {" "}
-          ${book.price} x {book.quantity} = {book.quantity * book.price}
-        </span>
+    <div>
+      <div className="cartProduct">
+        <div className="cartProductInfo">
+          <img src={book.imageUrl} className="cartProductImg" />
+          <span>{book.title}</span>
+          <span>{book.author}</span>
+          <span>
+            {" "}
+            ${book.price} x {book.quantity} = {book.quantity * book.price}
+          </span>
+        </div>
+        <button
+          type="button"
+          onClick={() => dispatch(deleteBook(book))}
+          className="deleteButton"
+        >
+          X
+        </button>
       </div>
-      <button
-        type="button"
-        onClick={() => dispatch(deleteBook(book))}
-        className="deleteButton"
-      >
-        X
-      </button>
-    </div>
+      quantity:
+      <input
+      id="quantity"
+      type="number"
+      min={1}
+      max={99}
+      defaultValue={book.quantity}
+      onChange={(e) => setQuantity(e.target.value)}
+    ></input>
+      <Link to='/cart'><button>Apply</button></Link>
+  </div>
   );
 };
