5241844889c22bcfe5580e63c8d59006e5113d37
santiagojon <jonwsantiago@gmail.com>
2022-06-20 12:02:23 -0400

    small changes to allBooks filter style

--- client/components/AllBooks.js
+++ client/components/AllBooks.js
@@ -4,8 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchAllBooks } from "../store/Books";
 
 const AllBooks = () => {
-  let numOfBooks = 12
-  const pathName = window.location.pathname
+  let numOfBooks = 12;
+  const pathName = window.location.pathname;
   const { book } = useSelector((state) => {
     return state;
   });
@@ -18,80 +18,98 @@ const AllBooks = () => {
     dispatch(fetchAllBooks());
   }, []);
 
-  if(pathName === '/currentTitles'){
-    numOfBooks = book.length
+  if (pathName === "/currentTitles") {
+    numOfBooks = book.length;
   }
 
   return (
     <div>
+      <span id="sortOptions">Sort options:</span>
       <select
-      name="sort"
-      onChange={(e) => setSort(e.target.value)}
-      type='text'
-      defaultValue='sort By'>
-        <option value='TitleA'>Title: A-Z</option>
-        <option value='TitleD'>Title: Z-A</option>
-        <option value='PriceA'>Price: Low-High</option>
-        <option value='PriceD'>Price: High-Low</option>
+        id="sortMenu"
+        name="sort"
+        onChange={(e) => setSort(e.target.value)}
+        type="text"
+        defaultValue="sort By"
+      >
+        <option value="TitleA">Title: A-Z</option>
+        <option value="TitleD">Title: Z-A</option>
+        <option value="PriceA">Price: Low-High</option>
+        <option value="PriceD">Price: High-Low</option>
       </select>
-      {sorting === 'PriceA' ? (
-      <div id="all-books">
-        {book.slice(0, numOfBooks).sort((a, b) => (a.price - b.price)).map((book) => (
-          <div key={book.id}>
-            <Link to={`/books/${book.id}`}>
-              <img src={book.imageUrl} className="grow" />
-              <h6>{book.title}</h6>
-              <h6>{book.author}</h6>
-            </Link>
-          </div>
-        ))}
-      </div>):(sorting === 'PriceD') ? (
-      <div id="all-books">
-        {book.slice(0, numOfBooks).sort((a, b) => (b.price - a.price)).map((book) => (
-          <div key={book.id}>
-            <Link to={`/books/${book.id}`}>
-              <img src={book.imageUrl} className="grow" />
-              <h6>{book.title}</h6>
-              <h6>{book.author}</h6>
-            </Link>
-          </div>
+      {sorting === "PriceA" ? (
+        <div id="all-books">
+          {book
+            .slice(0, numOfBooks)
+            .sort((a, b) => a.price - b.price)
+            .map((book) => (
+              <div key={book.id}>
+                <Link to={`/books/${book.id}`}>
+                  <img src={book.imageUrl} className="grow" />
+                  <h6>{book.title}</h6>
+                  <h6>{book.author}</h6>
+                </Link>
+              </div>
+            ))}
+        </div>
+      ) : sorting === "PriceD" ? (
+        <div id="all-books">
+          {book
+            .slice(0, numOfBooks)
+            .sort((a, b) => b.price - a.price)
+            .map((book) => (
+              <div key={book.id}>
+                <Link to={`/books/${book.id}`}>
+                  <img src={book.imageUrl} className="grow" />
+                  <h6>{book.title}</h6>
+                  <h6>{book.author}</h6>
+                </Link>
+              </div>
+            ))}
+        </div>
+      ) : sorting === "TitleA" ? (
+        <div id="all-books">
+          {book
+            .slice(0, numOfBooks)
+            .sort((a, b) => (a.title > b.title ? 1 : -1))
+            .map((book) => (
+              <div key={book.id}>
+                <Link to={`/books/${book.id}`}>
+                  <img src={book.imageUrl} className="grow" />
+                  <h6>{book.title}</h6>
+                  <h6>{book.author}</h6>
+                </Link>
+              </div>
+            ))}
+        </div>
+      ) : sorting === "TitleD" ? (
+        <div id="all-books">
+          {book
+            .slice(0, numOfBooks)
+            .sort((a, b) => (b.title > a.title ? 1 : -1))
+            .map((book) => (
+              <div key={book.id}>
+                <Link to={`/books/${book.id}`}>
+                  <img src={book.imageUrl} className="grow" />
+                  <h6>{book.title}</h6>
+                  <h6>{book.author}</h6>
+                </Link>
+              </div>
+            ))}
+        </div>
+      ) : (
+        <div id="all-books">
+          {book.slice(0, numOfBooks).map((book) => (
+            <div key={book.id}>
+              <Link to={`/books/${book.id}`}>
+                <img src={book.imageUrl} className="grow" />
+                <h6>{book.title}</h6>
+                <h6>{book.author}</h6>
+              </Link>
+            </div>
           ))}
         </div>
-      ) : (sorting === 'TitleA') ? (
-        <div id="all-books">
-        {book.slice(0, numOfBooks).sort((a, b) => (a.title > b.title) ? 1 : -1).map((book) => (
-          <div key={book.id}>
-            <Link to={`/books/${book.id}`}>
-              <img src={book.imageUrl} className="grow" />
-              <h6>{book.title}</h6>
-              <h6>{book.author}</h6>
-            </Link>
-          </div>
-        ))}
-      </div>
-      ): (sorting === 'TitleD') ? (
-        <div id="all-books">
-        {book.slice(0, numOfBooks).sort((a, b) => (b.title > a.title) ? 1 : -1).map((book) => (
-          <div key={book.id}>
-            <Link to={`/books/${book.id}`}>
-              <img src={book.imageUrl} className="grow" />
-              <h6>{book.title}</h6>
-              <h6>{book.author}</h6>
-            </Link>
-          </div>
-        ))}
-      </div>): (
-       <div id="all-books">
-        {book.slice(0, numOfBooks).map((book) => (
-          <div key={book.id}>
-            <Link to={`/books/${book.id}`}>
-              <img src={book.imageUrl} className="grow" />
-              <h6>{book.title}</h6>
-              <h6>{book.author}</h6>
-            </Link>
-          </div>
-        ))}
-      </div>)}
+      )}
     </div>
   );
 };
--- client/components/CartProduct.js
+++ client/components/CartProduct.js
@@ -1,4 +1,4 @@
-import React ,{ useState }from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { deleteBook } from "../store/cart";
@@ -9,18 +9,34 @@ export const CartProduct = (props) => {
   let book = props.book;
 
   const [quantity, setQuantity] = useState(book.quantity);
-  book.quantity = quantity
+  book.quantity = quantity;
   return (
     <div>
       <div className="cartProduct">
-      <img src={book.imageUrl} className="cartProductImg" />
-      <div className="cartProductInfo">
-        <span className="cartProductTitle">{book.title}</span>
-        <span className="cartProductAuthor">{book.author}</span>
-        <span className="cartProductPriceQty">
-          {" "}
-          ${book.price} - Quantity: {book.quantity}
-        </span>
+        <img src={book.imageUrl} className="cartProductImg" />
+        <div className="cartProductInfo">
+          <span className="cartProductTitle">{book.title}</span>
+          <span className="cartProductAuthor">{book.author}</span>
+          <span className="cartProductPriceQty">
+            {" "}
+            ${book.price} 
+            {/* - Quantity: {book.quantity} */}
+          </span>
+          <div>
+            Quantity:  
+            <input
+              id="quantity"
+              type="number"
+              min={1}
+              max={99}
+              defaultValue={book.quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            ></input>
+            <Link to="/cart">
+              <button className='changeQtyBtn'>Apply</button>
+            </Link>
+          </div>
+        </div>
         <button
           type="button"
           onClick={() => dispatch(deleteBook(book))}
@@ -29,17 +45,6 @@ export const CartProduct = (props) => {
           X
         </button>
       </div>
-      </div>
-      quantity:
-      <input
-      id="quantity"
-      type="number"
-      min={1}
-      max={99}
-      defaultValue={book.quantity}
-      onChange={(e) => setQuantity(e.target.value)}
-    ></input>
-      <Link to='/cart'><button>Apply</button></Link>
-  </div>
+    </div>
   );
 };
